cc9ee4170c9287dd7133b4755ed1fae4f3e69f17
int0x80-sys <matt.sosa314@gmail.com>
2023-02-16 21:16:23 +0000

    [feat]: expose the Checker abstract class to allow custom Checkers implementations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import Checker from "./checker";
 import IsArray from "./is_array_checker";
 import IsBoolean from "./is_boolean_checker";
 import IsNull from "./is_null_checker";
@@ -7,6 +8,7 @@ import IsString, { StringCheckerConstraint } from "./is_string_checker";
 import IsTuple, { TupleToCheckerTuple } from "./is_tuple_checker";
 
 export {
+	Checker,
 	IsNull,
 	IsNumber,
 	NumberCheckerConstraint,
